d547399e91eceed2566ba2be00dbe8aee7d81f25
joelrnorris <joel.r.norris@gmail.com>
2018-04-02 13:52:22 -0500

    initia commit

---
+++ OLA7_LinqToList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OLA7_LinqToList
+{
+
+    class Program
+    { 
+
+        static void Main(string[] args) { 
+
+            try  
+            {
+
+                    List<Student> students = File.ReadAllLines(@"./csvDBData.txt")
+                                               .Skip(1)                         // skip the header line
+                                               .Select(s => Student.FromCsv(s))
+                                               .ToList();
+
+                // loop through list and print them out.
+                /**
+                foreach (Student student in students)
+                {
+                    Console.WriteLine(student.ToString());
+                }
+                **/
+
+                // sort the students by GPA
+                students.Sort((x, y) => x.gpa.CompareTo(y.gpa));
+
+                // print out the top 10 students in the list
+                // for (int i = 0; i < students.Count; i++)
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.WriteLine(students[i].ToString());
+                }
+
+
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("csvDBData.txt not found");
+                Console.WriteLine(e.ToString());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+    }
+
+}
+
+
---
+++ OLA7_LinqToList/Student.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLA7_LinqToList
+{
+    class Student
+    {
+        // Student Name,Class,Earned Hours,GPA,Course,CourseNumber,FinalGrade,TotalCSCICreditHrs,CSCI GPA
+        // "Gomez, Christine",SR,124,3.0673,CSCI1170,1170,B,36,2.8144
+
+        public string lastName;
+        public string firstName;
+        public string classLevel;
+        public int earnedHours;
+        public double gpa;
+        public string course;
+        public int courseNumber;
+        public string finalGrade;
+        public int totalCSCICreditHours;
+        public double cscigpa;
+        
+        public static Student FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+
+            Student student = new Student();
+
+            student.lastName = values[0];
+            student.firstName = values[1];
+            student.classLevel = values[2];
+
+            try
+            {
+                student.earnedHours = Convert.ToInt32(values[3]);
+            }
+            catch(FormatException)
+            {
+                student.earnedHours = 0;
+            }
+
+            try
+            {
+                student.gpa = Convert.ToDouble(values[4]);
+            }
+            catch(FormatException)
+            {
+                student.gpa = 0.0;
+            }
+
+            student.course = values[5];
+
+            try
+            {
+                student.courseNumber = Convert.ToInt32(values[6]);
+            }
+            catch(FormatException)
+            {
+                student.courseNumber = 0;
+            }
+
+            student.finalGrade = values[7];
+
+            try
+            {
+                student.totalCSCICreditHours = Convert.ToInt32(values[8]);
+            }
+            catch(FormatException)
+            {
+                student.totalCSCICreditHours = 0;
+            }
+
+            try
+            {
+                student.cscigpa = Convert.ToDouble(values[9]);
+            }
+            catch(FormatException)
+            {
+                student.cscigpa = 0.0;
+            }
+
+            return student;
+
+        }
+
+        public override string ToString()
+        {
+
+            string studentString = 
+
+                lastName + ", " +
+                firstName + ", " +
+                classLevel + ", " +
+                Convert.ToString(earnedHours) + ", " +
+                Convert.ToString(gpa) + ", " +
+                course +
+                Convert.ToString(courseNumber) + ", " +
+                finalGrade + ", " +
+                Convert.ToString(totalCSCICreditHours) + ", " +
+                Convert.ToString(cscigpa) + ", ";
+
+            return studentString;
+        }
+
+    }
+
+}
